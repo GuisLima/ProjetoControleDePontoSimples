c903cd679feba83d0da636a5628eb18be5d7e24c
Guilherme <lima.guilhermerj@gmail.com>
2025-10-28 07:36:38 -0300

    Adiciona estrutura base do gerenciador

---
+++ Models/GerenciadorFuncionario.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoPonto.Models
+{
+    public class GerenciadorFuncionario
+    {
+        public List<Funcionario> listagemFuncionarios = new List<Funcionario>();
+    }
+}
